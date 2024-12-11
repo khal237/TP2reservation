74d36f1b642cb79ccb54c09c671c52da4e2ac941
Ibrahim Khalil Mounleon Ndam <im446728@ad.u-bourgogne.fr>
2024-12-11 11:43:45 +0100

    4 eme commit

--- Clienttest.cpp
+++ Clienttest.cpp
@@ -1,42 +1,11 @@
 #include "Client.h"
 #include <iostream>
 
-Client::Client(int id, const string& nom, const string& prenom)
-    : idclient(id), nom(nom), prenom(prenom) {}
+int main() {
+
+    Client client1(1, "khalil", "ndam");
+    client1.affichageclient();
 
 
-//nos getters
- int Client getidclient(){
- 	return idclient;
- }
-
- string Client getnom(){
- 	return getnom;
- }
-
- String Client getprenom(){
- 	return getprenom;
- }
-
-
-//nos setters 
- void Client::setidclient(int newid){
- 	idclient = newid;
- }
-
- void Client::setnom(int newnom){
- 	nom = newnom;
- }
- 
-  void Client::setprenom(int newprenom){
- 	prenom = newprenom;
- }
- 
- 
-void Client::affichageclient() const {
-    cout << "ID Client: " << getidclient << endl;
-    cout << "Nom: " << getnom << endl;
-    cout << "Prénom: " << getprenom << endl;
-
+return 0;
 }
-
